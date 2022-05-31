59ce4cf95102913fc0d4dd47d7a74d51e9c7f7e2
Joel Gustafson <joelg@mit.edu>
2022-05-31 09:09:33 -0400

    upgrade circom-lezer, move highlighting out

--- src/index.ts
+++ src/index.ts
@@ -6,54 +6,7 @@ import {
 	delimitedIndent,
 } from "@codemirror/language"
 
-import { styleTags, tags } from "@codemirror/highlight"
-
 import { parser } from "lezer-circom"
-
-// CM6 has a very constrained vocabulary of tags
-//   templates    -> tags.className
-//   components   -> tags.typeName
-//   variables    -> tags.variableName
-//   signals      -> tags.propertyName
-//   functions    -> tags.function(tags.variableName)
-//   constraints  -> tags.compareOperator
-// Call/Identifier, Assignment/Identifier, and Value/Identifier
-// are all just tags.name for now.
-const styleNodeProp = styleTags({
-	"include pragma circom main": tags.keyword,
-	"log assert": tags.operatorKeyword,
-	"input output public parallel": tags.modifier,
-	"if else for while do return": tags.controlKeyword,
-	"signal component var template function": tags.definitionKeyword,
-	LineComment: tags.lineComment,
-	BlockComment: tags.blockComment,
-	CompilerVersion: tags.literal,
-	Number: tags.number,
-	String: tags.string,
-
-	// Syntax (none of these are style by default)
-	"OpenBrace CloseBrace": tags.brace,
-	"OpenBracket CloseBracket": tags.bracket,
-	"OpenParen CloseParen": tags.paren,
-	'";"': tags.punctuation,
-
-	// Operators (none of these are style by default)
-	'"&" "|" "~" "^" ">>" "<<"': tags.bitwiseOperator,
-	'"+" "-" "*" "**" "/" "\\\\" "%"': tags.arithmeticOperator,
-	'"&&" "||" "!" "<" "<=" ">" ">=" "==" "!=="': tags.logicOperator,
-	'"===" "<==" "==>"': tags.compareOperator,
-
-	"TemplateDeclaration/Identifier FunctionDeclaration/Identifier Call/Identifier":
-		tags.function(tags.variableName),
-
-	"InputSignalDeclaration/Identifier OutputSignalDeclaration/Identifier IntermediateSignalDeclaration/Identifier":
-		tags.definition(tags.propertyName),
-	"PublicSignalsList/Identifier": tags.definition(tags.propertyName),
-	"Value/Signal/Identifier": tags.propertyName,
-
-	"ComponentDeclaration/Identifier VariableDeclaration/Identifier Assignment/Identifier Value/Identifier":
-		tags.variableName,
-})
 
 export const circomLanguage = LRLanguage.define({
 	parser: parser.configure({
@@ -69,8 +22,6 @@ export const circomLanguage = LRLanguage.define({
 					return { from: tree.from + 2, to: tree.to - 2 }
 				},
 			}),
-
-			styleNodeProp,
 		],
 	}),
 	languageData: {
